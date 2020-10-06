dc36c44d99b1da28b51941e2eb5ea9a210ebc946
lakeibt <lakeibt15@gmail.com>
2020-10-06 13:17:04 +0900

    commit cho

---
+++ app/src/main/java/com/example/ko_desk/myex_10/activity/WorkCheck.java
@@ -0,0 +1,25 @@
+package com.example.ko_desk.myex_10.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.example.ko_desk.myex_10.R;
+
+
+public class WorkCheck extends Activity {
+
+    Button r;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nfc);
+
+
+    }
+
+}
