3955e1ad245a94ba876f0bebe01d14831e5fc279
ChoiByeongHyun <choihyun7000@naver.com>
2020-10-06 18:47:40 +0900

    ssi8

---
+++ app/src/main/java/com/example/ko_desk/myex_10/vo/nfcVO.java
@@ -0,0 +1,36 @@
+package com.example.ko_desk.myex_10.vo;
+
+public class nfcVO {
+	
+	private int num;
+	private String pnum;
+	private String tagintime;
+	private String tagouttime;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getPnum() {
+		return pnum;
+	}
+	public void setPnum(String pnum) {
+		this.pnum = pnum;
+	}
+	public String getTagintime() {
+		return tagintime;
+	}
+	public void setTagintime(String tagintime) {
+		this.tagintime = tagintime;
+	}
+	public String getTagouttime() {
+		return tagouttime;
+	}
+	public void setTagouttime(String tagouttime) {
+		this.tagouttime = tagouttime;
+	}
+	
+	
+}
